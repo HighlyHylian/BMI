4365b623044b225f19370e337010059b219d6374
HighlyHylian <mdhdrck@gmail.com>
2025-03-18 21:39:10 -0500

    Finish functions, add new function test

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,12 +2,20 @@ def calculate_bmi(heightMeters, weightKg):
     return weightKg / (heightMeters ** 2)
 
 def get_kg_from_pounds(pounds):
-    pass
+    return pounds * 0.453592
 
 def get_meters_from_inches(inches):
-    pass
+    return inches * 0.0254
 
 def split_height_string(height):
+    try:
+        feet, inches_part = height.split("'")
+        inches = inches_part.replace('"', '')
+        return int(feet), int(inches)
+    except ValueError:
+        return None
+    
+def return_bmi_category(bmi):
     pass
 
 def main():
--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ from bmi_calculator import (
     get_kg_from_pounds,
     get_meters_from_inches,
     split_height_string,
+    return_bmi_category
 )
 
 class TestBMICalculator(unittest.TestCase):
@@ -16,22 +17,26 @@ class TestBMICalculator(unittest.TestCase):
         self.assertAlmostEqual(calculate_bmi(1.8, 90), 27.78, places=2)
 
     # Verify correct conversion from pounds to kg
-    # def test_get_kg_from_pounds(self):
-    #     self.assertAlmostEqual(get_kg_from_pounds(150), 68.04, places=2)
-    #     self.assertAlmostEqual(get_kg_from_pounds(100), 45.36, places=2)
-    #     self.assertAlmostEqual(get_kg_from_pounds(200), 90.72, places=2)
+    def test_get_kg_from_pounds(self):
+        self.assertAlmostEqual(get_kg_from_pounds(150), 68.04, places=2)
+        self.assertAlmostEqual(get_kg_from_pounds(100), 45.36, places=2)
+        self.assertAlmostEqual(get_kg_from_pounds(200), 90.72, places=2)
 
     # # Verify correct conversion from inches to meters
-    # def test_get_meters_from_inches(self):
-    #     self.assertAlmostEqual(get_meters_from_inches(70), 1.78, places=2)
-    #     self.assertAlmostEqual(get_meters_from_inches(60), 1.52, places=2)
-    #     self.assertAlmostEqual(get_meters_from_inches(72), 1.83, places=2)
+    def test_get_meters_from_inches(self):
+        self.assertAlmostEqual(get_meters_from_inches(70), 1.78, places=2)
+        self.assertAlmostEqual(get_meters_from_inches(60), 1.52, places=2)
+        self.assertAlmostEqual(get_meters_from_inches(72), 1.83, places=2)
     
     # Verify correct splitting of height string
-    # def test_split_height_string(self):
-    #     self.assertEqual(split_height_string("5'10"), (5, 10))
-    #     self.assertEqual(split_height_string("6'2"), (6, 2))
-    #     self.assertEqual(split_height_string("5'0"), (5, 0))
+    def test_split_height_string(self):
+        self.assertEqual(split_height_string("5'10\""), (5, 10))
+        self.assertEqual(split_height_string("6'2\""), (6, 2))
+        self.assertEqual(split_height_string("5'0\""), (5, 0))
+        self.assertEqual(split_height_string("411"), (None))
+
+    def test_return_bmi_category(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
